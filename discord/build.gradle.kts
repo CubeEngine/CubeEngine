4c361d3bd861c660daccc6f5714aea020cdbf56c
Anselm Brehme <development.anselm@breh.me>
2023-10-04 01:01:58 +0200

    discord4j netty relocate

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -1,8 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("org.cubeengine.parent.module")
 }
 
+tasks.withType<ShadowJar>().configureEach {
+    relocate("io.netty", "org.cubeengine.module.discord.reloacted.netty")
+}
+
 dependencies {
     implementation("com.discord4j:discord4j-core:3.2.6")
-// TODO shadow + relocate io.netty -> org.cubeengine.module.discord.reloacted.netty
 }
