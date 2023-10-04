4a04240d411b362312b126178b14299e5da34178
Phillip Schichtel <phillip@schich.tel>
2023-10-05 00:41:08 +0200

    update observe's netty version and drop opentracing

--- observe/build.gradle.kts
+++ observe/build.gradle.kts
@@ -8,11 +8,9 @@ dependencies {
 
     implementation("org.spongepowered:observer:1.0-SNAPSHOT")
     // Exposition HTTPServer
-    implementation("io.netty:netty-codec-http:4.1.82.Final")
+    implementation("io.netty:netty-codec-http:4.1.93.Final")
     // Monitoring
     implementation("io.prometheus:simpleclient:$prometheusVersion")
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-    // Tracing
-    implementation("io.opentracing:opentracing-api:0.33.0")
 }
