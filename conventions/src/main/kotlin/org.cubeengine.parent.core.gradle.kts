05f039ae30c973231d3605783c59effb39e36c25
Anselm Brehme <development.anselm@breh.me>
2024-04-14 19:36:27 +0200

    who needs sources anyways

--- conventions/src/main/kotlin/org.cubeengine.parent.core.gradle.kts
+++ conventions/src/main/kotlin/org.cubeengine.parent.core.gradle.kts
@@ -111,7 +111,6 @@ tasks.withType<JavaCompile>().configureEach {
 
 val projectJvmTarget = "17"
 java {
-    withSourcesJar()
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(projectJvmTarget))
         vendor.set(JvmVendorSpec.ADOPTIUM)
