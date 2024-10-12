bb64f38e5ddd946df02e0870936cd252caff7ecc
Anselm Brehme <development.anselm@breh.me>
2024-10-12 05:04:05 +0200

    fix netty dep

--- observe/build.gradle.kts
+++ observe/build.gradle.kts
@@ -9,7 +9,19 @@ dependencies {
 
     implementation("org.spongepowered:observer:1.0-SNAPSHOT")
     // Exposition HTTPServer
-    implementation("io.netty:netty-codec-http:4.1.93.Final")
+    implementation("io.netty:netty-codec-http:4.1.97.Final") {
+        exclude(group = "io.netty", module = "netty-codec")
+        exclude(group = "io.netty", module = "netty-transport")
+        exclude(group = "io.netty", module = "netty-common")
+        exclude(group = "io.netty", module = "netty-buffer")
+        exclude(group = "io.netty", module = "netty-handler")
+    }
+    compileOnly("io.netty:netty-codec:4.1.97.Final")
+    compileOnly("io.netty:netty-buffer:4.1.97.Final")
+    compileOnly("io.netty:netty-transport:4.1.97.Final")
+    compileOnly("io.netty:netty-common:4.1.97.Final")
+    compileOnly("io.netty:netty-handler:4.1.97.Final")
+    testImplementation("io.netty:netty-codec:4.1.97.Final")
     // Monitoring
     implementation("io.prometheus:simpleclient:$prometheusVersion")
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
